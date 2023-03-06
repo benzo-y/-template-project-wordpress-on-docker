9bdbda232570f1f9b1944c10dc347b4a92efacc2
benzo-y <fukubenzo@gmail.com>
2023-03-06 13:58:58 +0900

    display thumbnail

--- src/wp-content/themes/myblog-handson/single.php
+++ src/wp-content/themes/myblog-handson/single.php
@@ -12,7 +12,15 @@
   <?php if (have_posts()) : ?>
     <?php while (have_posts()) : the_post(); ?>
       <!-- Page Header -->
-      <header class="masthead" style="background-image: url('img/post-bg.jpg')">
+      <?php
+      if (has_post_thumbnail()) :
+        $id = get_post_thumbnail_id();
+        $img = wp_get_attachment_image_src($id, 'large');
+      else :
+        $img = array(get_template_directory_uri() . '/img/post-bg.jpg');
+      endif;
+      ?>
+      <header class="masthead" style="background-image: url('<?php echo $img[0]; ?>')">
         <div class="overlay"></div>
         <div class="container">
           <div class="row">
@@ -33,6 +41,7 @@
         <div class="container">
           <div class="row">
             <div class="col-lg-8 col-md-10 mx-auto">
+              <?php the_post_thumbnail(array(32, 32), array('alt' => 'アイキャッチ画像')); ?>
               <?php the_content(); ?>
             </div>
           </div>
